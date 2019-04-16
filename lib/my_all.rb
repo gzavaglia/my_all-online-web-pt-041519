01658f340ace9f28811d7d03547b92002d1f0403
Giovana Zavaglia <gio.zavaglia@gmail.com>
2019-04-16 15:30:40 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -7,4 +7,10 @@ def my_all?(collection)
     block_return_values << yield(collection[i])
     i += 1 
   end #end while 
+  
+  if block_return_values.include?(false)
+    false
+  else
+    true 
+  end #end if 
 end
